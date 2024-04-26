603adbdfcdabd3595771fbe501a423494163f671
Kytnacode <kytnacode@gmail.com>
2024-04-26 14:28:23 -0700

    Add ping endpoint

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/kytnacode/simple-crud/internal/handlers"
+)
+
 func main() {
-	println("Hello world")
+	log.Println("Starting server")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", handlers.Ping)
+
+	log.Fatal(http.ListenAndServe(":3000", mux))
 }
