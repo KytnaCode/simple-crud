8ce8ce614d4a985b2502d797fabb5835ddd182c3
Kytnacode <105133752+KytnaCode@users.noreply.github.com>
2024-05-03 10:35:06 -0700

    Register post routes

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,20 +4,22 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/joho/godotenv"
 	"github.com/kytnacode/simple-crud/internal/handlers"
-  "github.com/joho/godotenv"
 )
 
 func main() {
-  err := godotenv.Load()
+	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
-  
+
 	log.Println("Starting server")
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", handlers.Ping)
+
+	handlers.RegisterPostRoutes(mux)
 
 	log.Fatal(http.ListenAndServe(":3000", mux))
 }
